1e952a5766fc232d4b14b16d2503cffaef525ded
Emir Ziyan <emirziyan@gmail.com>
2026-02-09 20:15:28 +0000

    stage5 except Q3

--- mynitcbase/Schema/Schema.cpp
+++ mynitcbase/Schema/Schema.cpp
@@ -1,4 +1,28 @@
 #include "Schema.h"
-
+#include "../Cache/OpenRelTable.h"
 #include <cmath>
 #include <cstring>
+#include "../define/constants.h"
+
+
+int Schema::openRel(char relName[ATTR_SIZE]){
+    int ret=OpenRelTable::openRel(relName);
+    if(ret>0 || ret<=MAX_OPEN){
+        return SUCCESS;
+    }
+    return  ret;
+}
+
+int Schema::closeRel(char relName[ATTR_SIZE]) {
+    if(strcmp(relName,RELCAT_RELNAME) == 0 || strcmp(relName,ATTRCAT_RELNAME) == 0) {
+        return E_NOTPERMITTED ;
+    }
+
+    int relId = OpenRelTable::getRelId(relName);
+
+    if(relId == E_RELNOTOPEN) {
+        return E_RELNOTOPEN ;
+    }
+
+    return OpenRelTable::closeRel(relId);
+}
